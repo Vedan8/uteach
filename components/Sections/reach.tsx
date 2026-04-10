51f5a33461e318242f1c08a0fbabaf1006b21c1e
Vedan <vedanjyadav@gmail.com>
2026-04-10 13:04:16 +0530

    reach

--- components/Sections/reach.tsx
+++ components/Sections/reach.tsx
@@ -1,37 +1,31 @@
 import Image from "next/image";
-
-const stats = [
-  {
-    icon: "user-countries.png",
-  },
-  {
-    icon: "values-teachers.png",
-  },
-  {
-    icon: "happy-students.png",
-  },
-];
+import styles from "@/app/css/reach.module.css"
 
 export default function Reach() {
   return (
-    <section className="w-full bg-[#0f172a] py-14 md:py-20 font-roboto">
-      <div className="mx-auto flex max-w-5xl flex-col items-center justify-around gap-12 px-6 md:flex-row md:gap-6">
-        {stats.map((stat, i) => (
-          <div
-            key={i}
-            className="flex flex-col items-center gap-4 text-center"
-          >
-            <div className="h-100 w-100">
-              <Image
-                src={stat.icon}
-                alt={""}
-                width={200}
-                height={200}
-                className="h-full w-full object-contain"
-              />
-            </div>
-          </div>
-        ))}
+    <section className={styles.reach}>
+      <div className={styles.row}>
+        <Image
+          src={"/user-countries.png"}
+          alt="user_c"
+          height={213}
+          width={370}
+          className={styles.image}
+        />
+        <Image
+          src={"/values-teachers.png"}
+          alt="user_c"
+          height={213}
+          width={370}
+          className={styles.image}
+        />
+        <Image
+          src={"/happy-students.png"}
+          alt="user_c"
+          height={213}
+          width={370}
+          className={styles.image}
+        />
       </div>
     </section>
   );
